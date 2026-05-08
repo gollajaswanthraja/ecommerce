fb73671eaff70830498988fde02caff540d7a97b
Pavani Golla 23073238 <venkatapavanigolla@gmail.com>
2026-05-08 19:07:20 +0530

    firstcommit

---
+++ src/main/java/com/project/example/SbEcomApplication.java
@@ -0,0 +1,13 @@
+package com.project.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+
+}
