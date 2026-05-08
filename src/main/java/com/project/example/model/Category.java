fd274beff6f9f43f3b674b5cb0969ba7aa953434
Jaswanth Raja <gollajaswanthraja.gmail.com>
2026-05-08 19:28:20 +0530

    pom changes

--- src/main/java/com/project/example/model/Category.java
+++ src/main/java/com/project/example/model/Category.java
@@ -1,4 +1,13 @@
 package com.project.example.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Category {
+
+    private Long CategoryId;
+    private String CategoryName;
+
 }
