8ce2a9bc97a165b106ea6106d288771ea390f9e3
Jaswanth Raja <gollajaswanthraja.gmail.com>
2026-07-25 14:59:30 +0530

    added conversational memory

---
+++ src/main/java/com/project/example/ai/service/ConversationService.java
@@ -0,0 +1,4 @@
+package com.project.example.ai.service;
+
+public interface ConversationService {
+}
---
+++ src/main/java/com/project/example/ai/service/ConversationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.project.example.ai.service;
+
+public class ConversationServiceImpl {
+}
